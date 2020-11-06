f0d005e641c694ac1cb27cb81b7bf0e2f00be4fe
Lucas Santana <lucas.s.s.silva@hotmail.com>
2020-11-06 17:00:21 -0300

    First Model-Controller-View - Department

---
+++ SalesWebMVC/SalesWebMVC/Controllers/DepartmentsController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SalesWebMVC.Models.ViewModels;
+
+namespace SalesWebMVC.Controllers
+{
+    public class DepartmentsController : Controller
+    {
+        public IActionResult Index()
+        {
+
+            List<Department> list = new List<Department>();
+
+            list.Add(new Department { Id = 1, Name = "Eletronics" });
+            list.Add(new Department { Id = 2, Name = "Fashion" });
+
+            //enviar os dados da lista para a view
+            return View(list); //Enviando os dados do controller para a view
+        }
+    }
+}
---
+++ SalesWebMVC/SalesWebMVC/Models/ViewModels/Department.cs
@@ -0,0 +1,9 @@
+﻿namespace SalesWebMVC.Models.ViewModels
+{
+    public class Department
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+    }
+}
