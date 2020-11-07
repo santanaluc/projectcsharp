d68c1afc14dea534ef0343165a176dd8dd4a897c
Lucas Santana <lucas.s.s.silva@hotmail.com>
2020-11-07 02:54:21 -0300

    MySQL adaptation and first migration

--- SalesWebMVC/SalesWebMVC/Data/SalesWebMVCContext.cs
+++ SalesWebMVC/SalesWebMVC/Data/SalesWebMVCContext.cs
@@ -14,6 +14,6 @@ namespace SalesWebMVC.Data
         {
         }
 
-        public DbSet<SalesWebMVC.Models.Department> Department { get; set; }
+        public DbSet<Department> Department { get; set; }
     }
 }
