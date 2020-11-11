21f9ef0c28ce5aa161dffa94d3b11d3120f1ff27
Lucas Santana <lucas.s.s.silva@hotmail.com>
2020-11-11 10:49:06 -0300

    Validation

--- SalesWebMVC/SalesWebMVC/Models/Seller.cs
+++ SalesWebMVC/SalesWebMVC/Models/Seller.cs
@@ -9,17 +9,29 @@ namespace SalesWebMVC.Models
     public class Seller
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "{0} required")]
+        [StringLength(60,MinimumLength = 3, ErrorMessage = "{0} size should be between {2} and {1}")]
         public string Name { get; set; }
+
+        [Required(ErrorMessage = "{0} required")]
+        [EmailAddress(ErrorMessage = "Enter a valid {0}")]
         [DataType(DataType.EmailAddress)] //Transforma em um link de email
         public string Email { get; set; }
+
+        [Required(ErrorMessage = "{0} required")]
         //Customiza o que aparece na página
         [Display(Name = "Birth Date")]
         [DataType(DataType.Date)] // Formatação para só aparecer data
         [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]// dia/mes/ano
         public DateTime BirthDate { get; set; }
+        
+        [Required(ErrorMessage = "{0} required")]
+        [Range(100.0, 50000.0, ErrorMessage = "{0} must be from {1} to {2}")]
         [Display(Name = "Base Salary")]
         [DisplayFormat(DataFormatString = "{0:F2}")]
         public double BaseSalary { get; set; }
+        
         public Department Department { get; set; }
         public int DepartmentId { get; set; }
         public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
